6769d46a93c39a58209fadd8567b772e8cc9405b
Adopted Irelia <2822242638@qq.com>
2024-03-28 15:00:37 +0800

    kaibai

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -5,11 +5,57 @@ import torch
 from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 from skimage import io
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+import os 
+
+
+# 加载预训练的ResNet模型
+resnet_model = models.resnet18(pretrained=True)
+# 去掉最后一层全连接层
+resnet_model = nn.Sequential(*list(resnet_model.children())[:-1])
+# 设置为评估模式
+resnet_model.eval()
+
+# 图像预处理函数
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+def load_and_preprocess_image(img):
+    image = Image.fromarray(img)
+    #image = image.resize((512,512))
+    image = preprocess(image)
+    # 添加一个维度作为批处理维度
+    image = image.unsqueeze(0)
+    return image
+
+# 提取图像的特征
+def extract_features(image_path):
+    image = load_and_preprocess_image(image_path)
+    with torch.no_grad():
+        features = resnet_model(image)
+    return features.squeeze().numpy()
+
+# 计算两张图像之间的ID相似度
+# 越高越好
+def id_similarity(image1, image2):
+    features1 = extract_features(image1)
+    features2 = extract_features(image2)
+    # 计算余弦相似度
+    similarity = np.dot(features1, features2) / (np.linalg.norm(features1) * np.linalg.norm(features2))
+    return similarity
+
 
 # 越低越好
 def mse(image1, image2):
     # 确保图像尺寸相同
-    print(image1.shape,image2.shape)
+    #print(image1.shape,image2.shape)
     
     assert image1.shape == image2.shape
     
@@ -47,25 +93,58 @@ def ms_ssim(image1, image2):
 
 
 if __name__ == '__main__':
+    
+    lst = ['ours.jpg','out_e4e.jpg','out_SG2.jpg','out_SG2_p.jpg']
 
-    ori = cv2.imread('./img/ori.jpg')
-    res = cv2.imread('./img/res.png')
-    #res = cv2.imread('./img/out_e4e.jpg')
+    dic_mse = {'ours':[],'out_e4e':[],'out_SG2':[],'out_SG2_p':[]}
+    dic_ssim = {'ours':[],'out_e4e':[],'out_SG2':[],'out_SG2_p':[]}
+    dic_lpips = {'ours':[],'out_e4e':[],'out_SG2':[],'out_SG2_p':[]}
+    dic_id = {'ours':[],'out_e4e':[],'out_SG2':[],'out_SG2_p':[]}
+    
+    
+    for title in os.listdir('./img'):
+    
+        if title == '.DS_Store':
+            continue
+        ori = cv2.imread(f'./img/{title}/origin.jpg')
+        
+        for i in lst:
+            print("********************************")
+            print(f"计算{i}的指标")
 
-    ori= cv2.resize(ori, (512, 512))
-    res= cv2.resize(res, (512, 512))
+            res = cv2.imread(f'./img/{title}/{i}')
+            #res = cv2.imread('./img/out_e4e.jpg')
 
 
-# 计算MSE
-    mse_value = mse(ori, res)
+        # 计算MSE
+            mse_value = mse(ori, res)
 
-    print("MSE:", mse_value)
+            #print("MSE:", mse_value)
 
 
-    lpips_distance_value = lpips_distance(ori, res)
+            lpips_distance_value = lpips_distance(ori, res)
 
-    print("LPIPS Distance:", lpips_distance_value)
+            #print("LPIPS Distance:", lpips_distance_value)
 
-    ms_ssim_score = ms_ssim(ori, res)
+            ms_ssim_score = ms_ssim(ori, res)
 
-    print("MS-SSIM Score:", ms_ssim_score)
+            #print("MS-SSIM Score:", ms_ssim_score)
+
+            ID_value = id_similarity(ori,res)
+
+            print(f"MSE: {mse_value}")
+            print(f"LPIPS: {lpips_distance_value}")
+            print(f"SSIM: {ms_ssim_score}")
+            print(f"id similarity: {ID_value}")
+            
+            dic_mse[i.split('.')[0]].append(mse_value)
+            dic_lpips[i.split('.')[0]].append(lpips_distance_value)
+            dic_ssim[i.split('.')[0]].append(ms_ssim_score)
+            dic_id[i.split('.')[0]].append(ID_value)
+
+    print(dic_mse)
+    print(dic_lpips)
+    print(dic_ssim)
+    print(dic_id)
+
+
---
+++ evaluate/preprocess.py
@@ -0,0 +1,41 @@
+from PIL import Image 
+import os 
+
+
+if __name__ == '__main__':
+    title = 'Asian_male'
+    directory = f'./img/{title}'
+    if not os.path.exists(directory):
+        try:
+            # 创建目录
+            os.makedirs(directory)
+            print(f"目录 '{directory}' 创建成功")
+        except OSError as e:
+            # 打印错误信息
+            print(f"创建目录 '{directory}' 失败: {e}")
+    else:
+        print(f"目录 '{directory}' 已存在")
+
+    image1 = Image.open('./temp/ori.jpg')
+    image1 = image1.resize((512,512))
+    image1.save(f'{directory}/origin.jpg')
+
+
+    image1 = Image.open('./temp/res.png')
+    image1 = image1.resize((512,512))
+    image1.save(f'{directory}/ours.jpg')
+
+
+    image1 = Image.open('./temp/out_e4e.jpg')
+    image1 = image1.resize((512,512))
+    image1.save(f'{directory}/out_e4e.jpg')
+
+
+    image1 = Image.open('./temp/out_SG2.jpg')
+    image1 = image1.resize((512,512))
+    image1.save(f'{directory}/out_SG2.jpg')
+
+
+    image1 = Image.open('./temp/out_SG2_plus.jpg')
+    image1 = image1.resize((512,512))
+    image1.save(f'{directory}/out_SG2_p.jpg')
